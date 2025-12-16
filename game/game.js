5fb1b7ac8367937d7a72666df5ca6aad550aa620
Jumper <jonnyschoen29@gmail.com>
2025-12-16 10:19:51 -0500

    Update game.js

--- game/game.js
+++ game/game.js
@@ -21,3 +21,8 @@ b.onclick=()=>updateDoc(gameRef,{question:d.question+1});
 ans.appendChild(b);
 });
 });
+
+export const questions = [
+{ q:"2+2?", a:["3","4","5","6"], c:1 },
+{ q:"JS runs where?", a:["Server","Browser","Both","None"], c:2 }
+];
