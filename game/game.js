3132d5e54459037203680d3d8e5ca26e8d569df5
Jumper <jonnyschoen29@gmail.com>
2025-12-16 10:18:28 -0500

    Update game.js

--- game/game.js
+++ game/game.js
@@ -1,3 +1,4 @@
-function answer() {
-  alert("Answered!");
-}
+export const questions = [
+{ q:"2+2?", a:["3","4","5","6"], c:1 },
+{ q:"JS runs where?", a:["Server","Browser","Both","None"], c:2 }
+];
